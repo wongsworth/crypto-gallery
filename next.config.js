0afb0f7bedbe89a381abb4ec4253171d9e89f994
wongsworth <ben@wongsworth.com>
2025-04-15 00:02:12 -0400

    Update dependencies and simplify Next.js config

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
-  distDir: 'dist',
-  images: {
-    unoptimized: true
-  },
-  trailingSlash: true,
-  basePath: ''
+  output: 'export'
 };
 
 module.exports = nextConfig; 
